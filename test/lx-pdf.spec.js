31a5fdd1d83332b2c903491af9fc56ce15951708
Thomas Scheibe <th.scheibe@litixsoft.de>
2013-08-21 17:35:39 +0200

    Tests und Grundkonvertierung.

--- test/lx-pdf.spec.js
+++ test/lx-pdf.spec.js
@@ -1,4 +1,4 @@
-/*global describe, it, expect*/
+/*global describe, it, expect, waits*/
 'use strict';
 
 var sut = require('../lib/lx-pdf.js')('');
@@ -24,17 +24,35 @@ describe('lx-pdf', function () {
     it('add some content', function() {
         expect(sut.getPageIndex()).toBe(1);
         expect(sut.addContent('address', 'AAA BBB\nCCC 08\nDDDDD EEEEEE')).toBeTruthy();
+        expect(sut.addContent('linebreak', 'This text is too width. And should be automatic break.')).toBeTruthy();
+
+        expect(function() {
+            sut.addContent('toosmall', 'Height is too small!');
+        }).toThrow();
+
+        expect(sut.addContent('date', '31.12.1234')).toBeTruthy();
         expect(sut.addContent('subject', 'Small Test')).toBeTruthy();
-        expect(sut.addContent('content', 'Content for Page 1')).toBeTruthy();
+
+
+        // add Table Data
+        expect(sut.addTable('tablecontent', [0, 1, 2, 3])).toBeFalsy();
+        expect(function() {
+            sut.addTable('content', [0, 1, 2, 3]);
+        }).toThrow();
+        expect(sut.addTable('table', [0, 1, 2, 3])).toBeTruthy();
 
         sut.addPage();
         expect(sut.getPageIndex()).toBe(2);
         expect(sut.addContent('address', 'There is no Address Section for Page 2')).toBeFalsy();
         expect(sut.addContent('content', 'Content for Page 2')).toBeTruthy();
+        sut.addPage();
+        expect(sut.getPageIndex()).toBe(3);
+        expect(sut.addContent('content', 'The same areas on page 3+.')).toBeTruthy();
 
         sut.save('Dummy.pdf', function(result) {
             expect(result).toBeNull();
         });
-    });
 
+        waits(1000);
+    });
 });
