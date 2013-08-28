29efb96118b5723dee439827b8fc430094b7351b
Thomas Scheibe <th.scheibe@litixsoft.de>
2013-08-28 13:29:06 +0200

    Tests

--- test/lx-pdf.spec.js
+++ test/lx-pdf.spec.js
@@ -1,7 +1,7 @@
 /*global describe, it, expect, waits*/
 'use strict';
 
-var sut = require('../lib/lx-pdf.js')('');
+var sut = require('../lib/lx-pdf.js')('test/templates/template.json');
 
 var bigTextNumberOne = 'Gerald Geisler wurde in eine im österreichischen Galopprennsport involvierte Familie geboren und kam dadurch schon früh in Kontakt mit Pferden. Seine erste Morgenarbeit ritt er fünfzehnjährig auf den Pferden des familieneigenen Rennstalles Hernstein an der Galopprennbahn Freudenau in Wien. Seine Karriere als Amateurrennreiter begann er im Alter von 17 Jahren siegreich auf Pink Floyd. Im Laufe der Zeit kamen Siege in Italien, Slowakei und der Schweiz hinzu. Den Höhepunkt seiner Amateurrennreiterkarriere bildet sicherlich sein Antreten für Österreich im Fegentri. Seine schulische Laufbahn beinhaltet die Matura am Wiedner Gymnasium, einige Semester Jus sowie den Abschluss eines kaufmännischen Kollegs.\n\nNach Ableisten seines Präsenzdienstes legte Gerald Geisler 2001 erfolgreich die Prüfung zum Erwerb einer Lizenz als Trainer für Galopprennpferde ab und nahm seine Tätigkeit als Trainer in der Wiener Freudenau auf. Einer zwischenzeitlichen Standortverlegung in die 2004 neu errichtete Anlage in Ebreichsdorf (Magna Racino) folgte mit Beginn der Saison 2005 der Wechsel an die Galopprennbahn Riem in München. Seit Februar 2010 hat er sein Trainingsquartier in Iffezheim. Von 2005 bis 2006 war er für den Fernsehsender Premiere Win, 2009 für Equi8 als Kommentator für nationale und internationale Pferderennen tätig.';
 var bigTextNumberTwo = '\n1956 wechselte Gorton zum Fernsehen und entwarf für das Programm ABC Weekend Television die Szenenbilder zu der Reihe Armchair Theatre. Seit Mitte der 1960er Jahre designte Assheton Gorton primär für den Kinofilm. Seit er von Richard Lester für das moderne Zeitbild Der gewisse Kniff die architektonische Gestaltung vornahm, hat Gorton ein quantitativ schmales Œuvre vorgelegt, das jedoch nahezu durchgehend ambitioniert und hochklassig ist. Seine frühen Arbeiten wie der Fotografen-Krimi Blow Up oder die Popkultur-Spielerei Magic Christian machten ihn zu einem wichtigen Vertreter des Swinging Sixties-Kinos seines Landes. In späteren Jahren konzentrierte sich Assheton Gorton mehr und mehr auf die Ausstattung prunkvoller Bilderbögen und Ausstattungsepen wie das Historiendrama Revolution mit Al Pacino und der Fantasyfilm Legend mit dem jungen Tom Cruise.';
@@ -19,9 +19,9 @@ describe('lx-pdf', function () {
         expect(typeof sut.addImage).toBe('function');
     });
 
-    it('should be loads a template', function() {
+    it('should be loads a wrong template', function() {
         expect(sut.loadTemplate('nonexistfile.json')).toBeFalsy();
-        expect(sut.loadTemplate('test/templates/template.json')).toBeTruthy();
+        expect(sut.loadTemplate('')).toBeFalsy();
     });
 
     it('add some content and save to file', function() {
@@ -76,40 +76,44 @@ describe('lx-pdf', function () {
     });
 
     it('simple pdf', function() {
-//        var template = {
-//            name   : 'Dummy Template for Test',
-//            options: {
-//                pages: [
-//                    {
-//                        layout    : {
-//                            size   : "A4",
-//                            layout : "portrait",
-//                            margins: {
-//                                top   : 0,
-//                                left  : 0,
-//                                bottom: 0,
-//                                right : 0
-//                            }
-//                        },
-//                        sections: {
-//                            "content": {
-//                                "font"  : {
-//                                    "name" : "./test/fonts/arial.ttf",
-//                                    "size" : 12,
-//                                    "color": "#000000"
-//                                },
-//                                "format": {
-//                                    "align" : "left",
-//                                    "left"  : 70,
-//                                    "top"   : 350,
-//                                    "width" : 481,
-//                                    "height": 320
-//                                }
-//                            }
-//                        }
-//                    },
-//
-//        }
+        var simpleTemplate = {
+            name   : 'Dummy Template for Test',
+            options: {
+                pages: [
+                    {
+                        layout    : {
+                            size   : 'A4',
+                            layout : 'portrait',
+                            margins: {
+                                top   : 0,
+                                left  : 0,
+                                bottom: 0,
+                                right : 0
+                            }
+                        },
+                        sections: {
+                            content: {
+                                font  : {
+                                    name : './test/fonts/arial.ttf',
+                                    size : 12,
+                                    color: '#000000'
+                                },
+                                format: {
+                                    align : 'left',
+                                    left  : 70,
+                                    top   : 350,
+                                    width : 481,
+                                    height: 320
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        };
+
+        sut.loadTemplate(simpleTemplate);
+        sut.addContent('content', 'Hello World!');
 
         sut.print(function(result) {
             expect(result).toBeDefined();
