5c74828f24730a045dd3faa5c8f3b7cc5a7181ee
Andreas Krummsdorf <a.krummsdorf@litixsoft.de>
2014-01-21 11:05:19 +0100

    chore: add .jshintrc
    - move test files in test folder

--- test/lx-pdf.spec.js
+++ test/lx-pdf.spec.js
@@ -171,7 +171,7 @@ describe('lx-pdf', function () {
 
         sut.addContent('area51', 'A Image above');
 
-        sut.save('Dummy.pdf', function (result) {
+        sut.save('test/Dummy.pdf', function (result) {
             expect(result).toBeNull();
         });
 
@@ -235,7 +235,7 @@ describe('lx-pdf', function () {
         sut.addContent('content', '\nUt wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.');
         sut.addContent('content', '\nNam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer');
 
-        sut.save('invoice.pdf', function (result) {
+        sut.save('test/invoice.pdf', function (result) {
             expect(result).toBeNull();
         });
 
